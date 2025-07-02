9a2dbf9b24c7939f861d893ee14f9c898f239da5
NoonMutaz <168477584+NoonMutaz@users.noreply.github.com>
2025-07-02 10:47:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes,Route } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
-import Home from "./pages/Home/home"
+import Home from "./pages/Home/Home"
 import Cart from "./pages/Cart/Cart"
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder"
 import Contact from "./pages/ContactUs/Contact"
