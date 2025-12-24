d6d20ad0593e8b18e2dab01ac34763ad88f72dc1
NoonMutaz <noonalmutaz002@gmail.com>
2025-12-24 11:50:18 +0300

    feat: add happyRestaurant project details to Projects-data

--- src/data/Projects-data.jsx
+++ src/data/Projects-data.jsx
@@ -1,5 +1,5 @@
 //import DustyBazaar from '../assets/DustyBazaar.png';
-//import happyResturant from '../assets/happyResturant.png';
+import happyResturant from '../assets/happyResturant.png';
 import VillaVista from '../assets/VillaVista.png';
 //import Portfolio from '../assets/portfolio.png'
 export const projects = [
@@ -11,14 +11,14 @@ export const projects = [
   //   alt: "Second-Hand Marketplace",
   //   link: "https://e-commerce-dusty-bazaar-web.vercel.app/"
   // },
-//   { id: 2,
-//     title: "Frontend Arabic Restaurant Website",
-// description:
-//     "React and bootstrap restaurant with a clean, responsive design. Add or remove single or multiple items from the cart. View selected items and total cost on the cart page. Simulate orders and track them on their personal profile page.This project demonstrates dynamic state management with React Context, component-based styling using Styled Components, and interface optimized for both desktop and mobile devices.",
-//     image: happyResturant,
-//     alt: "Arabic Restaurant Website",
-//     link: "https://happiness-restaurant-pc32.vercel.app",
-//   },
+  { id: 2,
+    title: "Frontend Arabic Restaurant Website",
+description:
+    "React and bootstrap restaurant with a clean, responsive design. Add or remove single or multiple items from the cart. View selected items and total cost on the cart page. Simulate orders and track them on their personal profile page.This project demonstrates dynamic state management with React Context, component-based styling using Styled Components, and interface optimized for both desktop and mobile devices.",
+    image: happyResturant,
+    alt: "Arabic Restaurant Website",
+    link: "https://happiness-restaurant-pc32.vercel.app",
+  },
  
     {
     id: 4,
