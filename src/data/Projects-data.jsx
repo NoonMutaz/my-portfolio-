62865a6c255bfe7d33b3f06931c3bc6809617af9
NoonMutaz <noonalmutaz002@gmail.com>
2026-03-25 11:06:03 +0300

    removed resturant project

--- src/data/Projects-data.jsx
+++ src/data/Projects-data.jsx
@@ -12,14 +12,14 @@ export const projects = [
   //   alt: "Second-Hand Marketplace",
   //   link: "https://e-commerce-dusty-bazaar-web.vercel.app/"
   // },
-  { id: 2,
-    title: "Frontend Arabic Restaurant Website",
-description:
-    "React and bootstrap restaurant with a clean, responsive design. Add or remove single or multiple items from the cart. View selected items and total cost on the cart page using context to avoid prps drilling. Simulate orders and track them on their personal profile page.This project demonstrates dynamic state management with React Context, component-based styling using Styled Components, and interface optimized for both desktop and mobile devices.",
-    image: happyResturant,
-    alt: "Arabic Restaurant Website",
-    link: "https://happiness-restaurant-pc32.vercel.app",
-  },
+//   { id: 2,
+//     title: "Frontend Arabic Restaurant Website",
+// description:
+//     "React and bootstrap restaurant with a clean, responsive design. Add or remove single or multiple items from the cart. View selected items and total cost on the cart page using context to avoid prps drilling. Simulate orders and track them on their personal profile page.This project demonstrates dynamic state management with React Context, component-based styling using Styled Components, and interface optimized for both desktop and mobile devices.",
+//     image: happyResturant,
+//     alt: "Arabic Restaurant Website",
+//     link: "https://happiness-restaurant-pc32.vercel.app",
+//   },
      {
     id: 3,
     title: "QAflow",
